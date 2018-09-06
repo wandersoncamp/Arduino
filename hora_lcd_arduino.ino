ec7d6c15a7a18c9733d7adde98a352cc15d4d7c9
Wanderson Pacheco <wandersoncamp1308@gmail.com>
2018-09-06 10:19:59 -0300

    Add files via upload

---
+++ hora_lcd_arduino.ino
@@ -0,0 +1,63 @@
+#include <LiquidCrystal.h>
+int seg;
+int segadd;
+int minadd;
+int min;
+int horadd;
+int hor;
+LiquidCrystal lcd(2,4,5,10,11,12,13);
+void setup() {
+  horadd = 0;   
+   hor = 0;    
+  minadd = 0;   
+   min = 0;        
+  segadd = 0;  
+   seg = 0;    
+  lcd.begin(16, 2);
+}
+void loop() {
+  lcd.print("    ");
+  lcd.print(horadd);
+  lcd.print(hor);
+  lcd.write(':');
+  lcd.print(minadd);
+  lcd.print(min);
+  lcd.write(':');
+  lcd.print(segadd);
+  lcd.print(seg);
+  delay(1000);
+  seg++;
+  if (seg >= 10) {
+    segadd++;
+    seg = 0;
+  }
+  if (segadd >= 6) {
+     segadd = 0;
+     seg = 0;
+     min++;
+  }
+  if (min >= 10) {
+    minadd++;
+    min = 0;
+  }
+  if (minadd >= 6) {
+   minadd = 0;
+    min = 0;
+    hor++;
+  }
+  if (hor >= 10) {
+    horadd++;
+    hor = 0;
+  }
+  if (horadd >= 2 && hor >= 4) {
+    horadd = 0;
+    hor = 0;
+    minadd= 0;
+    min = 0;
+    segadd = 0;
+    seg = 0;
+  }
+  lcd.clear();
+}
+
+
---
+++ mensagem_lcd_arduino.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(2,4,5,10,11,12,13);
+
+void setup(){
+lcd.begin(16,2);  
+lcd.print("SISTEMAS DIEGAIS"); 
+
+
+} 
+
+void loop() {
+
+lcd.noDisplay(); 
+delay(700); 
+lcd.display(); 
+delay(700);
+
+//lcd.scrollDisplayLeft(); passar a mensagem da esquerda para direita
+//delay(500);
+  
+}
